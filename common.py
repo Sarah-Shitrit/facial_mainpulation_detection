43226f5b496a31db0a946ee921ddc281dfa71f16
Sarah Shitrit <83277915+Sarah-Shitrit@users.noreply.github.com>
2023-04-17 17:29:54 +0300

    Add files via upload

---
+++ common.py
@@ -0,0 +1,4 @@
+"""Common constants."""
+OUTPUT_DIR = r'C:\Users\97254\Documents\msc\Computer Vision\Final project\solution-20230415\output'
+CHECKPOINT_DIR = r'C:\Users\97254\Documents\msc\Computer Vision\Final project\solution-20230415\checkpoints'
+FIGURES_DIR = r'C:\Users\97254\Documents\msc\Computer Vision\Final project\solution-20230415\figures'
